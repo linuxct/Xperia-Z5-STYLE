69e461fbaa1e4bfef34ff62099834664d236892e
Sergio Castell <sergiocastell@gmail.com>
2016-02-07 11:19:31 +0100

    Lots of changes

--- common/buildprop.sh
+++ common/buildprop.sh
@@ -9,6 +9,7 @@ if [ "`${GREP} ro.product.name=D2302 /system/build.prop`" ];
    cp /tmp/buildprop/dual/build.prop /system/
    cp /tmp/buildprop/dual/InCallUI.apk /system/priv-app/InCallUI/
    cp /tmp/buildprop/dual/TeleService.apk /system/priv-app/TeleService/
+   cp /tmp/buildprop/dual/Settings.apk /system/priv-app/Settings/
 else
    	# Xperia M2 HSPASS
 	if [ "`${GREP} ro.product.name=D2305 /system/build.prop`" ];
